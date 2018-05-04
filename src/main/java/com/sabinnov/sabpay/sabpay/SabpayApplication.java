cefc1e8dd206ec77db724a2f67b85563edb23104
Foinke Mamadou ka <Fohinke2323@gmail.com>
2018-05-04 11:19:06 +0000

    first commit

---
+++ src/main/java/com/sabinnov/sabpay/sabpay/SabpayApplication.java
@@ -0,0 +1,12 @@
+package com.sabinnov.sabpay.sabpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabpayApplication.class, args);
+	}
+}
